b72371d86f72d59f33a3d1cb192b3b7034a33ee2
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-05 14:05:29 +0530

    added setups

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
 // ADMIN ROUTES
 app.use(ADMIN_AUTH_BASE, adiminAuthRoute)
 app.use('/api/admin/role', roleAuth)
