edfc5d52804b597ba98d456d9226e772f29156ea
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-06 14:11:37 +0530

    address page added

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ app.get('/admin',(req,res)=>{
 })
 
 app.get('/user',(req,res)=>{
-    res.status(200).render('user/profile/index')
+    res.status(200).render('user/profile/address/index')
 })
 
 export default app
