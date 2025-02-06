d627e1d6db934428107edb0d1b40edc6acd1d747
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-06 15:07:26 +0530

    password Reset added

--- src/app.js
+++ src/app.js
@@ -92,7 +92,7 @@ app.get('/admin',(req,res)=>{
 })
 
 app.get('/user',(req,res)=>{
-    res.status(200).render('user/profile/address/create')
+    res.status(200).render('user/profile/password')
 })
 
 export default app
