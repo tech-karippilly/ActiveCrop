8219fe16093be0f1dfdd34e633c9640cdb2e99f6
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-07 19:43:35 +0530

    protected route added

--- src/controllers/app/profile/index.js
+++ src/controllers/app/profile/index.js
@@ -1,20 +1,19 @@
 import { HTTP_NOT_FOUND, HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../../constans/httpStatus.js";
 import { USER_PROFILE_PAGE } from "../../../constans/page.js";
 import { User } from "../../../models/index.js";
+import jwt from 'jsonwebtoken'
 
 export async function renderProfilePage(req, res) {
     try {
-        // const access_token = req.session.accessToken
+        const access_token = req.session.accessToken
+        if (access_token) {
+            const jwtDecode = jwt.verify(access_token, process.env.JWT_SECRET_ACCESS_TOKEN)
+            const userId = jwtDecode.userId
+            const currentUser = await User.findById(userId)
+            res.status(HTTP_SUCCESS).render(USER_PROFILE_PAGE, { currentUser })
+        }
 
-        // if(access_token){
-        //     const jwtDecode = jwt.verify(access_token, process.env.JWT_SECRET_ACCESS_TOKEN)
-        //     const userId = jwtDecode.userId
-        //     const currentUser = await User.findById(userId)
 
-        // }
-        const currentUser = await User.findById('67930bdbd933b5aa5b33d335')
-        
-        res.status(HTTP_SUCCESS).render(USER_PROFILE_PAGE, { currentUser })
     } catch (error) {
         res.status(HTTP_SERVER_ERROR).render(USER_PROFILE_PAGE)
     }
@@ -24,27 +23,36 @@ export async function updateProfileDetails(req, res) {
     try {
         const { firstName, lastName, userName, email, phone } = req.body
 
-        const currentUser = await User.findById('67930bdbd933b5aa5b33d335')
+        const access_token = req.session.accessToken
 
-        if (currentUser) {
+        if (access_token) {
+            const jwtDecode = jwt.verify(access_token, process.env.JWT_SECRET_ACCESS_TOKEN)
+            const userId = jwtDecode.userId
+            const currentUser = await User.findById(userId)
+            if (currentUser) {
 
-            currentUser.firstName = firstName
-            currentUser.lastName = lastName
-            currentUser.userName = userName
-            currentUser.email = email
-            currentUser.phone = phone
+                currentUser.firstName = firstName
+                currentUser.lastName = lastName
+                currentUser.userName = userName
+                currentUser.email = email
+                currentUser.phone = phone
 
 
-            if (req.file) {
-                const newPath = req.file.path.replace(/^src[\\/]/, '');
-                const filePath = `http://localhost:3000/${newPath}`
-                currentUser.profileImage = filePath
+                if (req.file) {
+                    const newPath = req.file.path.replace(/^src[\\/]/, '');
+                    const filePath = `http://localhost:3000/${newPath}`
+                    currentUser.profileImage = filePath
+                }
+
+                await currentUser.save()
+                return res.status(HTTP_SUCCESS).json({ message: "User Update Successfully" })
             }
-
-            await currentUser.save()
-            return res.status(HTTP_SUCCESS).json({ message: "User Update Successfully" })
+            return res.status(HTTP_NOT_FOUND).json({ message: "User Not Found" })
         }
-        return res.status(HTTP_NOT_FOUND).json({ message: "User Not Found" })
+
+
+
+        return res.status(HTTP_NOT_FOUND).json({ message: "Log in to continue" })
 
     } catch (error) {
         return res.status(HTTP_SERVER_ERROR).json({ message: "Internal Server Error" })
