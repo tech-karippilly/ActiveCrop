67358048d9d52d976e3864c1624b5614fed7cf08
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-23 00:36:03 +0530

    added wallet design

--- src/app.js
+++ src/app.js
@@ -26,8 +26,9 @@ import cartRoute from './routes/app/cart/index.js'
 import orderRoute from './routes/app/order/index.js'
 import adminOrderRoute from './routes/orders/index.js'
 import offerRoutes  from './routes/offers/index.js'
+import whishlistRoutes from './routes/whishlist/index.js'
 
-import { ADMIN_AUTH_BASE, ADMIN_CATAGOERY_BASE, ADMIN_CUSTOMER_BASE, ADMIN_OFFERS_BASE, ADMIN_ORDERS_BASE, ADMIN_PRODUCTS_BASE, ORDERS_BASE, USER_CART_BASE, USER_HOME, USER_LOGIN_BASE, USER_OTP_BASE, USER_PRODUCTS, USER_PROFILE } from "./constans/endpoints.js";
+import { ADMIN_AUTH_BASE, ADMIN_CATAGOERY_BASE, ADMIN_CUSTOMER_BASE, ADMIN_OFFERS_BASE, ADMIN_ORDERS_BASE, ADMIN_PRODUCTS_BASE, ORDERS_BASE, USER_CART_BASE, USER_HOME, USER_LOGIN_BASE, USER_OTP_BASE, USER_PRODUCTS, USER_PROFILE, WHISLIST_BASE } from "./constans/endpoints.js";
 import { OrderSuccess } from "./controllers/app/order/index.js";
 
 const app = express()
@@ -92,6 +93,7 @@ app.use(USER_PRODUCTS,userProductsRoute)
 app.use(USER_PROFILE,profileRoute)
 app.use(USER_CART_BASE,cartRoute)
 app.use(ORDERS_BASE,orderRoute)
+app.use(WHISLIST_BASE,whishlistRoutes)
 
 
 app.use('/page',pageRoute)
