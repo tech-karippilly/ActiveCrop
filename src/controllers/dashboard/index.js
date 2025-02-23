83b4ecf91506132dd1688a2ef0bf91d3b7bb8787
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-02-23 19:32:59 +0530

    added to dashboard

--- src/controllers/dashboard/index.js
+++ src/controllers/dashboard/index.js
@@ -1,6 +1,82 @@
+import { Order } from "../../models/index.js"
 
-function dasboardPage(req,res){
-    res.status(200).render('admin/dashboard/dasbboard')
+async function dasboardPage(req, res) {
+    try {
+
+        const totalOrders = await Order.aggregate([
+            {
+                $match: {
+                    paymentStatus: 'Paid',
+                }
+            },
+            {
+                $count: "totalOrders"
+            }
+        ]);
+
+        const pendingOrders = await Order.aggregate([
+            {
+                $match: {
+                    deliveryStatus: { $ne: 'Delivered' }
+                }
+            },
+            {
+                $count: 'totalOrders'
+            }
+        ])
+
+        const successOrders = await Order.aggregate([
+            {
+                $match: {
+                    paymentStatus: "Paid",
+                    deliveryStatus: 'Delivered'
+                }
+            },
+            {
+                $count: "totalOrders"
+            }
+        ]);
+
+        const totalAmount = await Order.aggregate([
+            {
+                $match: {
+                    paymentStatus: 'Paid',
+                    deliveryStatus: 'Delivered'
+                },
+            },
+
+            {
+                $group: {
+                    _id: null,
+                    totalAmount: {
+                        $sum: '$totalAmount'
+                    }
+                }
+            }
+        ]);
+
+        const totalDiscoutAmount = await Order.aggregate([
+            {
+                $match: {
+                    deliveryStatus: 'Paid',
+                    deliveryStatus: 'Delivered'
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    discount: {
+                        $sum: '$discount'
+                    }
+                }
+            }
+        ]);
+        
+        res.status(200).render('admin/dashboard/dasbboard',{totalOrders,totalDiscoutAmount,totalAmount,successOrders,pendingOrders})
+    } catch (error) {
+
+    }
+
 }
 
-export {dasboardPage}
+export { dasboardPage }
