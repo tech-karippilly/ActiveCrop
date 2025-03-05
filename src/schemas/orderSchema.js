2e7db3b1d53afc7d659bd4837fa79b05d0464582
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-03-05 13:18:40 +0530

    cancelation requested added

--- src/schemas/orderSchema.js
+++ src/schemas/orderSchema.js
@@ -67,7 +67,7 @@ const orderSchema = new mongoose.Schema({
   },
   deliveryStatus: {
     type: String,
-    enum: ['Pending', 'Shipped', 'Delivered', 'Cancelled','Retrun Order Processing','Returned'],
+    enum: ['Pending', 'Shipped', 'Delivered', 'Cancelation Requested','Cancelled', 'Retrun Order Processing','Returned'],
     default: 'Pending'
   },
   totalAmount: {
