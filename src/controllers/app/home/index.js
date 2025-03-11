0aee76e3d00a7c9aed48f730927de5f7f1317727
“tech-karippilly” <vishnukarippilly@gmail.com>
2025-03-11 15:25:01 +0530

    added top 10 products

--- src/controllers/app/home/index.js
+++ src/controllers/app/home/index.js
@@ -1,6 +1,6 @@
 import { HTTP_SERVER_ERROR, HTTP_SUCCESS } from "../../../constans/httpStatus.js"
 import { USER_HOME_PAGE } from "../../../constans/page.js"
-import { Cart, Categoery, User } from "../../../models/index.js"
+import { Cart, Categoery, Product, User } from "../../../models/index.js"
 import dotenv from 'dotenv'
 import jwt from 'jsonwebtoken'
 
@@ -9,6 +9,20 @@ const renderHomepage = async (req, res) => {
     try {
         const catagories = await Categoery.find()
         const access_token = req.session.accessToken
+        const topProducts = await Product.aggregate([
+            {
+              $match: {
+                status:'Available',isBlocked:false
+              }
+            },
+            {
+              $sort:{sales_count:-1}
+            },
+            {
+              $limit: 10
+            },
+            
+          ])
         if (access_token) {
             const jwtDecode = jwt.verify(access_token, process.env.JWT_SECRET_ACCESS_TOKEN)
             const userId = jwtDecode.userId
@@ -18,9 +32,15 @@ const renderHomepage = async (req, res) => {
             if (cart.length > 0 && cart[0].items) { 
                 cartLength = cart[0].items.length;
             }
-            return res.status(HTTP_SUCCESS).render(USER_HOME_PAGE, { isLogin: true, catagories, currentUser, cartLength })
+
+            
+
+            return res.status(HTTP_SUCCESS).render(USER_HOME_PAGE, { isLogin: true, catagories,topProducts, currentUser, cartLength })
         }
-        return res.status(HTTP_SUCCESS).render(USER_HOME_PAGE, { isLogin: false, catagories, currentUser: {} })
+
+
+
+        return res.status(HTTP_SUCCESS).render(USER_HOME_PAGE, { isLogin: false, catagories,topProducts, currentUser: {} })
     } catch (error) {
         res.status(HTTP_SERVER_ERROR).render(USER_HOME_PAGE, { isLogin: false, catagories: [], currentUser: {} })
     }
